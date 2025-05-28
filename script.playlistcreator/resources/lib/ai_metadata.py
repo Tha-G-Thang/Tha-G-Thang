a8d5518188ccb63a14b205554553694b2b0ba7e5
ET_Design <132652726+EricTiemes@users.noreply.github.com>
2025-05-28 04:34:28 +0200

    Delete script.playlistcreator/resources/lib/ai_metadata.py

--- script.playlistcreator/resources/lib/ai_metadata.py
+++
@@ -1,9 +0,0 @@
-from mutagen import File as AudioFile
-from resources.lib.utils import log
-
-class AIMetadata:
-    def extract_audio_metadata(self, filepath):
-        audio = AudioFile(filepath)
-        metadata = {key: audio.get(key, '') for key in audio.keys()}
-        log(f"Extracted metadata from {filepath}: {metadata}")
-        return metadata
